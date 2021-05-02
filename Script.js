f569fd3cb9be33f1483971ee1adb7d7660afaaf7
Michael Parras <michaelparras1632@gmail.com>
2021-05-01 20:43:54 -0600

    Initial  Commit

---
+++ Script.js
@@ -0,0 +1,31 @@
+var a = 3;
+var b = 4;
+var sum = a + b;
+console.log(sum);
+
+var c = 4;
+var d = 2;
+var sub = c - d;
+console.log(sub);
+
+var e = 5;
+var f = 1;
+var div = e / f;
+console.log(div);
+
+var w = 7;
+var x = 9;
+var mult = x * w;
+console.log(mult);
+
+var string1 = "Hello";
+var string2 = " ";
+var string3 = "World";
+var combineStrings = string1 + string2 + string3;
+console.log(combineStrings);
+
+var stringW = "My favorite food is";
+var stringX =" ";
+var stringY ="Chinese!";
+var combineStrings = stringW + stringX + stringY;
+console.log(combineStrings);
